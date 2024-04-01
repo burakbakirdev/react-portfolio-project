20cc1f474666a589c9f5ba6c6e3c9bd14709dd21
burakbakirdev <iburakbakir@gmail.com>
2024-04-01 17:20:24 +0300

    finished

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react"
+
+import {SectionWrapper} from '../hoc'
+
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default SectionWrapper(Contact, '') 
